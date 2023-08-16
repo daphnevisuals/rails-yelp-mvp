612e74af22237194ee3715be1b1289e4ba7e4b11
Daphne Staruch <daphstr@gmail.com>
2023-08-16 16:39:50 -0400

    controller restaurants with index, new and create methods & views

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,37 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
+  # get /restaurants
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  # GET "restaurants/new"
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def show
+  end
+
+  # POST /restaurants
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to @restaurant, notice: "Restaurant was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+end
